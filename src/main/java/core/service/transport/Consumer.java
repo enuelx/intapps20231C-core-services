ab9bb63cb19419dc3f783c14b9ce45a87f239419
BertiRean <tplaza15@gmail.com>
2023-05-01 13:09:19 -0300

    Messages: Added Classes for handling of Messages

--- src/main/java/core/service/transport/Consumer.java
+++ src/main/java/core/service/transport/Consumer.java
@@ -1,14 +1,25 @@
 package core.service.transport;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import core.service.transport.server.IncommingMessage;
+import core.service.transport.server.OutputMessage;
 
 public class Consumer {
-public Consumer(){
+  @Autowired
+  private SimpMessagingTemplate socketTemplate;
 
-}
+  public Consumer(){
 
-@RabbitListener(queues = {RabbitConfig.TRADING_QUEUE})
-public void consume(String in){
-  System.out.println("Message Consumed from Transport queue: " + in);
-}  
+  }
+
+  @RabbitListener(queues = {RabbitConfig.TRADING_QUEUE})
+  public void consume(String in){
+    System.out.println("Message Consumed from Transport queue: " + in);
+
+    IncommingMessage ret = new IncommingMessage(in, "Trading");
+    socketTemplate.convertAndSend("/topic/users", ret);
+  }
 }
--- src/main/java/core/service/transport/server/OutputMessage.java
+++ src/main/java/core/service/transport/server/OutputMessage.java
@@ -2,8 +2,23 @@ package core.service.transport.server;
 
 public class OutputMessage {
 
+  private String from;
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  private String content;
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
   public String getContent(){
-    return new String("");
+    return content;
   }
   
 }
