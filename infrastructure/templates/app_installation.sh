88759b15a3556695ddc922a8bd31d763aec19c34
Emmanuel <emma.maidana@gmail.com>
2023-05-06 21:11:06 -0300

    add awslogs

--- infrastructure/templates/app_installation.sh
+++ infrastructure/templates/app_installation.sh
@@ -5,4 +5,5 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-mkdir /home/ec2-user/app
+sudo mkdir /home/ec2-user/app
+sudo chown -R ec2-user /home/ec2-user/app
